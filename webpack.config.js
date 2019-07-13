9f5a3650647a925e31e92f944f12ac260f8643b8
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2019-07-13 16:48:39 -0300

    Config eslint and usky

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    devtool: 'cheap-module-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                resolve: {
-                  extensions: ['.js', '.jsx'],
-                },
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            }
-        ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: 'index.html'
-      })
+  devtool: "cheap-module-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        resolve: {
+          extensions: [".js", ".jsx"]
+        },
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
     ]
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ]
+};
